6a3de07dead0802da5dcecf89f158fcadcfd6f96
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-05-13 20:17:31 -0600

    build

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 if (process.env.NODE_ENV === 'development') {
     const cors = require('cors')
     server.use(cors())
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>success</h1>')
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'hello' })
+})
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 4000

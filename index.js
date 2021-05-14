22ddf29f5699829ae7848eeb4c24fad637c7dd8d
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-05-13 18:04:27 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+require('dotenv').config()
+
 console.log("hello world")
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') {
+    server.use(express.json())
+    server.use(cors())
+}
